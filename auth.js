11b9a437a35f3db89bfcf8a2b6fa576e677c3b98
darsh.patel@einfochips.com <darsh.patel@einfochips.com>
2024-01-12 15:52:48 +0530

    Fixed authentication wording

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate(login, password) {
 	if(login === 'login' && password === 'password') {
-	  return 'You are loggen in';
+	  return 'You are loggen in!';
 	}else {
 	return 'Login is not correct';
 	}
