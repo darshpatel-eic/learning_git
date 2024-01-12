d90e3326b0d7c6b45b4a7146d0e1518b857b2d2a
Darsh Patel <156405126+darshpatel-eic@users.noreply.github.com>
2024-01-12 15:19:34 +0530

    2.js

--- 2.js
+++ 2.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 function getPerson(name,surname) {
-	return name + " " + surname;
+	return name + "<< >>" + surname;
 }
-=======
-function getPerson(name, surname) {
-	return name + " " + surname;
-}	
->>>>>>> mplement-new-logic
+
